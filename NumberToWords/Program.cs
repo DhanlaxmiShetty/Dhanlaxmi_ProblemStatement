34ab96e7878e8e35de901021d81edca2113253e3
Dhanlaxmi Shetty <dhanlaxmi.shetty@gmail.com>
2020-05-29 10:19:47 +0530

    Added functions to NumberWords project

--- NumberToWords/Program.cs
+++ NumberToWords/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NumberToWords
 {
     class Program
     {
+        static string ConvertNumberToWords(int number)
+        {
+            string strWords = String.Empty;
+
+            if (number == 0)
+            {
+                strWords = "Zero";
+            }
+            else
+            {
+                strWords = NumWords(number);
+            }
+
+            return strWords;
+        }
+
+        static string NumWords(int number)
+        {
+            return String.Empty;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a number to convert to words : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0}", ConvertNumberToWords(number));
+            Console.ReadLine();
         }
     }
 }
